4ec3fa302306ef421b2dd90ab0d355a5fe1e1f90
LIAM LE <lekongfuc@yahoo.com>
2019-08-01 21:08:49 -0700

    Add files via upload

---
+++ TalentApp/Talent.WpfClient/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Talent.WpfClient
+{
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Unexpected Exception",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = false;
+        }
+    }
+}
